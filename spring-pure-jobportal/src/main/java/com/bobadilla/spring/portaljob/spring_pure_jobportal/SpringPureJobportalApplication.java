0e5c00e3404bd08bb2c104249c285bd8326f1d49
Israel Bobadilla Segundo <israelbobadilla9@hotmail.com>
2025-10-07 19:11:36 -0600

    - Proyecto utilizando thymeleaf

---
+++ spring-pure-jobportal/src/main/java/com/bobadilla/spring/portaljob/spring_pure_jobportal/SpringPureJobportalApplication.java
@@ -0,0 +1,13 @@
+package com.bobadilla.spring.portaljob.spring_pure_jobportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPureJobportalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPureJobportalApplication.class, args);
+	}
+
+}
---
+++ spring-pure-jobportal/src/test/java/com/bobadilla/spring/portaljob/spring_pure_jobportal/SpringPureJobportalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bobadilla.spring.portaljob.spring_pure_jobportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPureJobportalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
