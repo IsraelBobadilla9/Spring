f1bf670e8aa50b33a51e09d3c109e0f1ea2249d2
Israel Bobadilla Segundo <israelbobadilla9@hotmail.com>
2025-10-07 18:46:57 -0600

    - AOP With join points

--- SpringAOP/core-aop/src/main/java/com/bobadilla/aop/core_aop/aop/MyAspect.java
+++ SpringAOP/core-aop/src/main/java/com/bobadilla/aop/core_aop/aop/MyAspect.java
@@ -1,5 +1,6 @@
 package com.bobadilla.aop.core_aop.aop;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
@@ -12,7 +13,12 @@ public class MyAspect {
     private static final Logger log = LoggerFactory.getLogger(MyAspect.class);
     
     @Before("execution(* com.bobadilla.aop.core_aop.aop.TargetObject.*(..))")
-    public void before(){
+    public void before(JoinPoint joinPoint){
+        log.info("Method name {}",joinPoint.getSignature().getName());// nombre del metodo que invoca el aspecto
+        log.info("Object type ",joinPoint.getSignature().getDeclaringTypeName());
+        log.info("modificadores ",joinPoint.getSignature().getModifiers());
+        log.info("Argumentos ",joinPoint.getArgs());
+
         log.info("Before advice");
     }
 
